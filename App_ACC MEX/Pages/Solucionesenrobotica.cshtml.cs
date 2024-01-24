1b15466cc3f0d6b09fe4bdeac740f1ca54c603ae
Garcia022 <garciap.rodrigo2204@gmail.com>
2024-01-24 12:11:46 -0600

    Creacion de vistas con sus archivos de diseño en css, asignacion de rutas para cada vista y su diseño.

---
+++ App_ACC MEX/Pages/Solucionesenrobotica.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace App_ACC_MEX.Pages
+{
+    public class SolucionesenroboticaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ App_ACC MEX/Pages/Vision.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace App_ACC_MEX.Pages
+{
+    public class VisionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
